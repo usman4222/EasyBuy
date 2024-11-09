fe1b61c23a1e29c9c8c6f2bf0549342d1a5e5d41
Rana Usman <usman4243ch@gmail.com>
2024-11-08 22:44:13 -0800

    Complete del product by admin api

--- routes/productRoute.js
+++ routes/productRoute.js
@@ -1,6 +1,6 @@
 import express from "express";
 import { authorizeRole, isAuthenticatedUser } from "../middleware/authUser.js";
-import { createProduct, getAdminProducts, getProducts, updateProduct } from "../controllers/productController.js";
+import { createProduct, deleteProduct, getAdminProducts, getProducts, updateProduct } from "../controllers/productController.js";
 
 const router = express.Router()
 
@@ -8,6 +8,7 @@ router.post('/admin/create-product',  isAuthenticatedUser, authorizeRole("admin"
 router.get('/get-product', getProducts )
 router.get('/admin/products',  isAuthenticatedUser, authorizeRole("admin"), getAdminProducts )
 router.put('/admin/update-product/:id',  isAuthenticatedUser, authorizeRole("admin"), updateProduct )
+router.delete('/admin/delete-product/:id',  isAuthenticatedUser, authorizeRole("admin"), deleteProduct )
 
 
 
